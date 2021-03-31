9687a2e45fabd1c58d79cede122088ad3c4918ce
Justijn Depover <justijndepover@gmail.com>
2021-03-31 16:06:04 +0200

    throw error with correct code

--- src/Exceptions/CouldNotAquireAccessTokenException.php
+++ src/Exceptions/CouldNotAquireAccessTokenException.php
@@ -8,6 +8,6 @@ class CouldNotAquireAccessTokenException extends Exception
 {
     public static function make(string $code, string $message): self
     {
-        return new static("Error $code: Could not aquire or refresh access token: $message");
+        return new static("Error $code: Could not aquire or refresh access token: $message", $code);
     }
 }
--- src/Exceptions/NoAccessToScopeException.php
+++ src/Exceptions/NoAccessToScopeException.php
@@ -8,6 +8,6 @@ class NoAccessToScopeException extends Exception
 {
     public static function make(string $code, string $message): self
     {
-        return new static("Error $code: $message");
+        return new static("Error $code: $message", $code);
     }
 }
